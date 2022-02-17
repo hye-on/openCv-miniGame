2596852c54b0db34a471e635ab42961cd0ac13f6
mythofsummer <joungeun0825@naver.com>
2022-02-17 17:46:20 +0900

    그 전보다 더 거리가 가까우면 실패

--- orangegame_joungeun.py
+++ orangegame_joungeun.py
@@ -63,27 +63,6 @@ rc2 = (x2, y2, w2, h2)
 
 ret, img = cap.read()
 
- 
-# roi = img[y:y+h, x:x+w]
-# roi_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-
-# roi2 = img[y2:y2+h2, x2:x2+w2]
-# roi_hsv2 = cv2.cvtColor(roi2, cv2.COLOR_BGR2HSV)
-
-# # HS 히스토그램 계산
-# channels = [0, 1]
-# ranges = [0, 180, 0, 256]
-# hist = cv2.calcHist([roi_hsv], channels, None, [90, 128], ranges)
-# hist2 = cv2.calcHist([roi_hsv2], channels, None, [90, 128], ranges)
-
-# # Mean Shift 알고리즘 종료 기준
-# #term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-# # CamShift 알고리즘 종료 기준
-# term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-
-# plt.hist(img.ravel(), 256, [0,256]); 
-# plt.show()
-
 while True:
 
     # x, y, w, h =115, 220, 50, 50
@@ -172,23 +151,6 @@ while True:
             img = cv2.flip(img,1)
             if not ret:
                 break
-            #cv2.rectangle(img, rc, (0, 0, 255), 2)
-            #cv2.rectangle(img, rc2, (0, 0, 255), 2)
-            #관심영역이 계속 바뀌는데 while을 돌때마다 초기화 해서 제대로 인식 못했던것.
-            # roi = img[y:y+h, x:x+w]
-            # roi_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-
-            # roi2 = img[y2:y2+h2, x2:x2+w2]
-            # roi_hsv2 = cv2.cvtColor(roi2, cv2.COLOR_BGR2HSV)
-
-            # # HS 히스토그램 계산
-            # channels = [0, 1]
-            # ranges = [0, 180, 0, 256]
-            # hist = cv2.calcHist([roi_hsv], channels, None, [90, 128], ranges)
-            # hist2 = cv2.calcHist([roi_hsv2], channels, None, [90, 128], ranges)
-
-            # # Mean Shift 알고리즘 종료 기준
-            # term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 
             # HS 히스토그램에 대한 역투영
             frame_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -197,36 +159,11 @@ while True:
            #삭제
             backproj2 = cv2.calcBackProject([frame_hsv], channels, hist2, ranges, 1)
 
-           # Mean Shift
-            # _, rc = cv2.meanShift(backproj, rc, term_crit)
-            # _, rc2 = cv2.meanShift(backproj2, rc2, term_crit)
-
  
             # CamShift
             #rc 입력이자 출력
             ret, rc = cv2.CamShift(backproj, rc, term_crit)
             ret2, rc2 = cv2.CamShift(backproj2, rc2, term_crit)
-
-            # 가까워질수록 커짐
-            #print(width)
-            #print(length)
-            
-            #거리 출력
-            #x1=ret[0][0]
-            #y1=ret[0][1]
-            #x2=ret2[0][0]
-            #y2=ret2[0][1]
-
-            #a=x2-x1
-            #b=y2-y1
-
-            #t3=math.sqrt((a*a)+(b*b))
-            
-            #sum=sum+t3
-            #count=count+1
-
-            #test3 = "Distance: "+ str(t3)
-            #cv2.putText(img, test3, (60,60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,255), 1)
 
             # 추적 결과 화면 출력
             cv2.rectangle(img, rc, (0, 0, 255), 2)
@@ -259,16 +196,26 @@ while True:
                     #(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             sum=sum+rd
             count=count+1
-            
+            test6=[0,0,0,0,0,0,0,0,0,0]
+            i=1
             # x키 누르면 손 사이 거리 평균 출력
             if cv2.waitKey(1) & 0xFF == ord('x'):
+                i+=1 
                 test5="This round: "
-                test6=str(sum/count)
-                ret2, img1 = cap.read()
-                img1 = cv2.flip(img1,1)
-                cv2.putText(img1, test5, (150,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,255), 3)
-                cv2.putText(img1, test6, (5,200), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,255), 3)   
-                cv2.imshow('a1', img1)
+                test6[i]=sum/count
+                print(test6)
+                if test6[i-1] < test6[i]:
+                    ret2, img1 = cap.read()
+                    fail="Fail!"
+                    cv2.putText(img1, fail, (150,300), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,255,255), 5)
+                    cv2.imshow('a1', img1)
+
+                else:
+                    ret2, img1 = cap.read()
+                    img1 = cv2.flip(img1,1)
+                    cv2.putText(img1, test5, (150,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 1)
+                    cv2.putText(img1, str(test6[i]), (5,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 1)   
+                    cv2.imshow('a1', img1)
            
             # Display the clicked frame for 2
             # sec.You can increase time in
