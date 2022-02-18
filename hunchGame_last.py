d5965773f8fa275ca49d746b94f8146e41f2685c
5nam <86754153+5nam@users.noreply.github.com>
2022-02-18 19:28:06 +0900

    눈치게임 완성
    파일 읽어오는데에서 알 수 없는 오류가 발생하여 임의로 초를 설정해 놓았고, 멀티프로세싱을 구현하지 못하여 임의로 설정한 초(sec) 데이터로 비교하여 승패 결정

---
+++ hunchGame_last.py
@@ -0,0 +1,192 @@
+import sys
+import numpy as np
+import cv2
+import time
+from datetime import datetime
+#now = time.localtime()
+
+file = open('time.txt', 'w')  # 시간 파일 저장
+file_v = open('time_v.txt', 'r', encoding='UTF8')
+head_count = 0
+
+global test_time
+
+# 비디오 파일 열기
+#cap = cv2.VideoCapture('camshift.avi')
+
+# open webcam (웹캠 열기)
+webcam = cv2.VideoCapture(0)
+
+Known_distance = 60  # centimeter
+Known_width = 14.3
+
+#Colors
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+WHITE = (255, 255, 255)
+fonts = cv2.FONT_HERSHEY_COMPLEX
+
+#cap = cv2.VideoCapture(0)  # 웹 카메라 열기
+
+# face detector object
+face_detector = cv2.CascadeClassifier("haarcascade_frontface.xml")
+
+# focal length finder function
+
+
+def FocalLength(measured_distance, real_width, width_in_rf_image):
+    focal_length = (width_in_rf_image * measured_distance)/real_width
+    return focal_length
+
+# distance estimation function
+
+
+def Distance_finder(Focal_Length, real_face_width, face_width_in_frame):
+    distance = (real_face_width * Focal_Length)/face_width_in_frame
+    return distance
+
+
+global rc
+
+
+def face_data(image):
+    face_width = 0
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
+
+    for (x, y, h, w) in faces:
+        global rc
+        cv2.rectangle(image, (x, y), (x+w, y+h), WHITE, 1)
+        rc = (x, y, w, h)
+        face_width = w
+
+    return face_width
+
+
+# reading reference image from directory
+ref_image = cv2.imread("Ref_image.jpg")
+
+ref_image_face_width = face_data(ref_image)
+Focal_length_found = FocalLength(Known_distance, Known_width, ref_image_face_width)
+print(Focal_length_found)
+#cv2.imshow("Ref_image", ref_image)
+
+if not webcam.isOpened():
+    print("Could not open webcam")
+    exit()
+
+#  if not cap.isOpened():
+#      print('Video open failed!')
+#      sys.exit()
+
+# 초기 사각형 영역: (x, y, w, h)
+
+ret, frame = webcam.read()
+
+"""
+x, y, w, h = cv2.selectROI(frame)
+rc = (x, y, w, h)
+print(rc)
+"""
+if not ret:
+    print('frame read failed!')
+    sys.exit()
+
+"""
+roi = frame[y:y+h, x:x+w]
+roi_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+
+
+# HS 히스토그램 계산
+channels = [0, 1]
+ranges = [0, 180, 0, 256]
+hist = cv2.calcHist([roi_hsv], channels, None, [90, 128], ranges)
+
+
+# Mean Shift 알고리즘 종료 기준
+term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+"""
+
+while webcam.isOpened():
+
+    # 비디오 매 프레임 처리
+    #while True:
+    #ret, frame = webcam.read()
+    _, frame = webcam.read()
+
+    face_width_in_frame = face_data(frame)
+    # finding the distance by calling function Distance finder
+    if face_width_in_frame != 0:
+        Distance = Distance_finder(
+            Focal_length_found, Known_width, face_width_in_frame)
+
+    # Drwaing Text on the screen
+        # cv2.putText(frame, f"Distance = {Distance}",
+        #             (50, 50), fonts, 0.6, (GREEN), 2)
+
+        if Distance < 160:
+            cv2.putText(frame, "back your step",
+                        (50, 80), fonts, 1.0, (RED), 3)
+        elif Distance > 210:
+            cv2.putText(frame, "front you step",
+                        (50, 80), fonts, 1.0, (RED), 3)
+
+    if rc[1] < 95:
+        global test
+        cv2.rectangle(frame, rc, (0, 225, 0), 2)
+        now = time.localtime()  # 닿은 순간 시간 계산하기 위해서
+        hour = now.tm_hour
+        min = now.tm_min
+        sec = now.tm_sec
+        #test = "Time : %d : %d : %d" % (hour, min, sec)
+        # 분을 초로 환산하여, 초가 더 적은 쪽이 우승.
+        if head_count < 1:
+            test = "Time : %d : %d : %d" % (hour, min, sec)
+            min = min*60
+            test_time = sec+min
+            file.write(str(test_time) + '\n')  # txt 파일에 시간 기록.
+            print(test_time)
+            head_count += 1
+        cv2.putText(frame, test, (50, 100),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    # 추적 결과 화면 출력
+    else:
+        if head_count < 1:
+            cv2.rectangle(frame, rc, (0, 0, 255), 2)
+        elif head_count >= 1:
+            vs_time = 5000 # 숫자 파일 읽어오고 싶은데, 자꾸 공백 없는데 공백 오류가 남...
+            if vs_time > test_time:
+                cv2.putText(frame, "YOUR WIN!", (100, 100),
+                            fonts, 3, (0, 255, 0), 4)
+            if vs_time < test_time:
+                cv2.putText(frame, "YOUR LOSE!", (100, 100),
+                            fonts, 3, (0, 255, 0), 4)
+            if vs_time == test_time:
+                cv2.putText(frame, "TIE!", (100, 100),
+                            fonts, 3, (0, 255, 0), 4)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print(test)
+        file.close()
+        break
+    if not ret:
+        break
+
+    """
+    # HS 히스토그램에 대한 역투영
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    backproj = cv2.calcBackProject([frame_hsv], channels, hist, ranges, 1)
+
+    # Mean Shift
+    _, rc = cv2.meanShift(backproj, rc, term_crit)
+
+    #draw = ImageDraw.Draw(frame)
+    #print("가나다라마바")
+    
+    """
+
+
+webcam.release()
+cv2.destroyAllWindows()
